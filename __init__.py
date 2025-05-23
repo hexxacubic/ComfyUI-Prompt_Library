9215d2b6fb5b78f162694653b21ac211813ae366
hexxacubic <cubehexagonal@gmail.com>
2025-05-23 20:46:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
-from .Prompt_Library import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .prompt_library_node import Prompt_Library
+
+NODE_CLASS_MAPPINGS = {
+    "Prompt_Library": Prompt_Library,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Prompt Library": "Prompt_Library",
+}
