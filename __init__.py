d2e55e85bc4193337df7f21318d34ea8cdc057e7
hexxacubic <cubehexagonal@gmail.com>
2025-05-24 20:09:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-from .Prompt_Library import Prompt_Library
+from .Prompt_Library import Prompt_Library, Random_Project
 
 NODE_CLASS_MAPPINGS = {
     "Prompt_Library": Prompt_Library,
+    "Random_Project": Random_Project,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Prompt_Library": "Prompt Library",
+    "Random_Project": "Random Project",
 }
