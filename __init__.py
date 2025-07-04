5d1cb999c008a7249033b87ac02851cebe01c305
hexxacubic <cubehexagonal@gmail.com>
2025-07-04 21:49:20 +0200

    Update __init__.py
    Added Multi Wildcard Loader

--- __init__.py
+++ __init__.py
@@ -2,12 +2,14 @@ from .Prompt_Library import Prompt_Library
 from .Simple_Prompt_Library import Simple_Prompt_Library
 from .Prompt_Extender import Prompt_Extender
 from .Double_Prompt_Encode import Double_Prompt_Encode
+from .Multi_Wildcard_Loader import Multi_Wildcard_Loader
 
 NODE_CLASS_MAPPINGS = {
     "Prompt_Library": Prompt_Library,
     "Simple_Prompt_Library": Simple_Prompt_Library,
     "Prompt_Extender": Prompt_Extender,
     "Double_Prompt_Encode": Double_Prompt_Encode,
+    "Multi_Wildcard_Loader": Multi_Wildcard_Loader,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -15,4 +17,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Simple_Prompt_Library": "Simple Prompt Library",
     "Prompt_Extender": "Prompt Extender",
     "Double_Prompt_Encode": "Double Prompt Encode",
+    "Multi_Wildcard_Loader": "Multi Wildcard Loader",
 }
